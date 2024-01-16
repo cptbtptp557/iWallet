0e102ac1b9d023b60c47767d1e3c00cb7c67576a
lan2557 <1215758259@qq.com>
2024-01-16 21:54:28 +0800

    完成了静态展示页

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <p class="accountName">accountName1</p>
-  <el-menu
-      default-active="2"
-      class="el-menu-vertical-demo"
-      :collapse="isCollapse"
-  >
+  <el-menu class="el-menu-vertical-demo">
     <el-sub-menu index="1">
       <template #title>
         <el-icon>
@@ -12,33 +8,40 @@
         </el-icon>
         <span>首页</span>
       </template>
-      <el-menu-item-group>
-        <el-menu-item index="1-3">展示页</el-menu-item>
-      </el-menu-item-group>
+      <label from="home">
+        <el-menu-item-group>
+          <el-menu-item index="1-1" :style="{ color: showcasePage === home ? '#3f9dfd' : '' }">展示页</el-menu-item>
+          <input type="radio" v-model="showcasePage" :value="home" id="home" style="display: none">
+        </el-menu-item-group>
+      </label>
     </el-sub-menu>
-    <el-menu-item index="4">
-      <el-icon>
-        <setting/>
-      </el-icon>
-      <template #title>设置</template>
-    </el-menu-item>
+    <label from="setUp">
+      <el-menu-item index="2" :style="{ color: showcasePage === setUp ? '#3f9dfd' : '' }">
+        <input type="radio" v-model="showcasePage" :value="setUp" id="setUp" style="display: none">
+        <el-icon>
+          <Setting/>
+        </el-icon>
+        <template #title>设置</template>
+      </el-menu-item>
+    </label>
   </el-menu>
 
   <keep-alive class="show">
-    <component :is="home"></component>
+    <component :is="showcasePage"></component>
   </keep-alive>
 </template>
 
 <script setup>
-import home from './components/home/home.vue'
+import {shallowRef} from "vue";
+import home from './components/home/home.vue';
+import setUp from "./components/setUp/setUp.vue";
 
 import {
   House,
   Setting,
-} from '@element-plus/icons-vue'
-import {ref} from "vue";
+} from '@element-plus/icons-vue';
 
-const isCollapse = ref(false);
+const showcasePage = shallowRef(home);
 </script>
 
 <style>
--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,13 +1,57 @@
 <template>
-<h1>{{isCollapse}}</h1>
+  <div class="outside">
+    <div class="balance">
+      <p>余额GoerliETH</p>
+    </div>
+    <div class="toolbar">
+      <div class="public">
+        <p>公钥</p>
+      </div>
+      <div class="actionBar">
+        <el-row>
+          <el-tooltip content="转账">
+            <el-button :icon="Sort" size="large" circle style="transform: rotate(90deg)"/>
+          </el-tooltip>
+          <el-tooltip content="刷新">
+            <el-button :icon="RefreshRight" size="large" circle style="transform: rotate(90deg)"/>
+          </el-tooltip>
+          <el-tooltip content="添加代币">
+            <el-button :icon="Plus" size="large" circle style="transform: rotate(90deg)"/>
+          </el-tooltip>
+        </el-row>
+      </div>
+    </div>
+    <el-divider/>
+    <div class="dataDisplay">
+      <el-table :data="lists" max-height="350" border>
+        <el-table-column prop="tradingResults" label="交易结果" width="200"/>
+        <el-table-column prop="amountTransaction" label="交易金额" width="300"/>
+        <el-table-column prop="tradingHours" label="交易时间"/>
+        <el-table-column label="更多数据">
+          <el-button>更多数据
+            <el-icon>
+              <Right/>
+            </el-icon>
+          </el-button>
+        </el-table-column>
+      </el-table>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import {home} from './home.js';
+import {Sort, RefreshRight, Plus, Right} from "@element-plus/icons-vue";
+import {ref} from "vue";
 
 const {isCollapse} = home();
+const lists = ref([{
+  tradingResults: '收款',
+  amountTransaction: '0.234' + 'GoerliETH',
+  tradingHours: new Date()
+}])
 </script>
 
-<style>
+<style scoped>
 @import "home.less";
 </style>
